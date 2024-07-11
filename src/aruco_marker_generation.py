5f0025fe526b4513f517bf7a6038b9a271f1c310
Giacomo Salici <giambo2000@gmail.com>
2024-07-12 00:25:29 +0200

    better export and config file

--- src/aruco_marker_generation.py
+++ src/aruco_marker_generation.py
@@ -1,10 +1,14 @@
 import cv2
 import cv2.aruco as aruco
+from pathlib import Path
+import tomllib
 
 def generate_aruco_marker(id, size, save_path):
     aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
     img = aruco.generateImageMarker(aruco_dict, id, size)
-    cv2.imwrite(save_path, img)
+    cv2.imwrite(str(save_path), img)
 
-generate_aruco_marker(42, 200, 'aruco_marker_42.png')
-generate_aruco_marker(1, 200, 'aruco_marker_01.png')
+config = tomllib.load(open('config.toml', 'rb'))   
+path = Path(config['aruco_export_path'])
+generate_aruco_marker(42, 200, path / 'aruco_marker_42.png')
+generate_aruco_marker(1, 200, path / 'aruco_marker_01.png')
