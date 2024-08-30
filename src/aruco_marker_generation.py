8895ffde59ec8b535602bcb74c872c4bad310651
Giacomo Salici <giambo2000@gmail.com>
2024-08-30 23:53:49 +0200

    more aruco

--- src/aruco_marker_generation.py
+++ src/aruco_marker_generation.py
@@ -10,5 +10,8 @@ def generate_aruco_marker(id, size, save_path):
 
 config = tomllib.load(open('config.toml', 'rb'))   
 path = Path(config['aruco_export_path'])
-generate_aruco_marker(42, 200, path / 'aruco_marker_42.png')
+generate_aruco_marker(249, 200, path / 'aruco_marker_249.png')
 generate_aruco_marker(1, 200, path / 'aruco_marker_01.png')
+generate_aruco_marker(2, 200, path / 'aruco_marker_02.png')
+generate_aruco_marker(3, 200, path / 'aruco_marker_03.png')
+generate_aruco_marker(4, 200, path / 'aruco_marker_04.png')
